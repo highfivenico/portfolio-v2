6b35e1b354f35d6191f605caa066618809608262
Nicolas HighFive <highfivenico@gmail.com>
2026-01-21 15:59:31 +0100

    feat: add About section

---
+++ src/components/About.jsx
@@ -0,0 +1,23 @@
+const About = () => {
+  // Rendu du composant
+  return (
+    <section className="about" id="about">
+      <div className="about__inner">
+        <div className="about__text">
+          <p>
+            J’aime travailler sur des projets variés mêlant design exigeant et
+            enjeux techniques.
+          </p>
+          <br />
+          <p>
+            Créer des expériences web vivantes et intuitives tout en explorant
+            de nouveaux défis techniques fait partie de ce qui me motive au
+            quotidien.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Hero from "../components/Hero";
 import Projects from "../components/Projects";
+import About from "../components/About";
 
 const Home = () => {
   return (
     <div className="app">
       <Hero />
       <Projects />
+      <About />
     </div>
   );
 };
